58d7acd0ebe051cd661c1b87026224f8fdae10ca
eddieKarlsson <eddieperty@gmail.com>
2021-12-02 10:05:33 +0100

    print statements

--- day2/solution.py
+++ day2/solution.py
@@ -13,37 +13,39 @@ def part1(input_file):
         elif cmd == 'up':
             depth_pos -= n_moves
 
-    print(f'Horizontal={horizontal_pos}\tDepth={depth_pos}')
+    print(f'Part1:\nHorizontal={horizontal_pos}\tDepth={depth_pos}')
 
     return horizontal_pos * depth_pos
 
 
 def part2(input_file):
-    with open(input_file) as f:
-        input_list = [int(line.rstrip()) for line in f]
+    horizontal_pos = 0
+    depth_pos = 0
+    aim_pos = 0
+    iteration = 0
+    for line in open(input_file):
+        cmd_list = line.split()
+        cmd = cmd_list[0]
+        n_moves = int(cmd_list[1])
 
-    # Create list of three summed values
-    sum3_list = []
-    for i, item in enumerate(input_list):
-        sum = item
-        if i < len(input_list) - 1:
-            sum += input_list[i + 1]
-        if i < len(input_list) - 2:
-            sum += input_list[i + 2]
-        sum3_list.append(sum)
-        # print(sum)
+        if cmd == 'forward':
+            horizontal_pos += n_moves
+            depth_pos = n_moves * aim_pos
+        elif cmd == 'down':
+            aim_pos += n_moves
+        elif cmd == 'up':
+            aim_pos -= n_moves
 
-    #  Count if the current summed value is greater than last
-    count = 0
-    previous = None
-    for current in sum3_list:
-        if previous and current > previous:
-            count += 1
-        previous = current
+        iteration += 1
+        print(
+            f'Iteration: {iteration}\thorizontal:{horizontal_pos}\tdepth:{depth_pos}\taim:{aim_pos}')
+        input("Press Enter to continue...")
 
-    return count
+    print(f'Part2:\nHorizontal={horizontal_pos}\tDepth={depth_pos}')
+
+    return horizontal_pos * depth_pos
 
 
 if __name__ == '__main__':
-    print(part1('input_test.txt'))
-    #  print(part2('input.txt'))
+    #  print(part1('input.txt'))
+    print(part2('input_test.txt'))
