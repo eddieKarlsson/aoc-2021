f4da4356caed334d33edd0df5adda9975856483e
eddieKarlsson <eddieperty@gmail.com>
2021-12-02 10:09:26 +0100

    finalize part2

--- day2/solution.py
+++ day2/solution.py
@@ -22,7 +22,6 @@ def part2(input_file):
     horizontal_pos = 0
     depth_pos = 0
     aim_pos = 0
-    iteration = 0
     for line in open(input_file):
         cmd_list = line.split()
         cmd = cmd_list[0]
@@ -30,16 +29,11 @@ def part2(input_file):
 
         if cmd == 'forward':
             horizontal_pos += n_moves
-            depth_pos = n_moves * aim_pos
+            depth_pos += n_moves * aim_pos
         elif cmd == 'down':
             aim_pos += n_moves
         elif cmd == 'up':
             aim_pos -= n_moves
-
-        iteration += 1
-        print(
-            f'Iteration: {iteration}\thorizontal:{horizontal_pos}\tdepth:{depth_pos}\taim:{aim_pos}')
-        input("Press Enter to continue...")
 
     print(f'Part2:\nHorizontal={horizontal_pos}\tDepth={depth_pos}')
 
@@ -48,4 +42,4 @@ def part2(input_file):
 
 if __name__ == '__main__':
     #  print(part1('input.txt'))
-    print(part2('input_test.txt'))
+    print(part2('input.txt'))
